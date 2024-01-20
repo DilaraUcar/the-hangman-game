f1a0a470f2dcd52c3d26a53be66a4fb211d037d8
DilaraUcar <>
2024-01-20 15:09:50 +0000

    Add draw hangman funstion and graphics for inccorect attempts

--- run.py
+++ run.py
@@ -36,3 +36,111 @@ def display_word(word, guessed_letters):
         else:
             display += "_"
     return display
+display_word(word, guessed_letters)
+
+def draw_hangman(incorrect_attempts):
+    """
+    Display hangman graphics based on incorrect attempts.
+    """
+    hangman_graphics = [
+        # 0 incorrect attempts:
+        """
+        -------
+        |   |
+        |
+        |
+        |
+        |
+        |
+        |
+        |
+        |
+        -------
+        """,
+        # 1 incorrect attempt:
+        """
+        -------
+        |   |
+        |   O
+        |
+        |
+        |
+        |
+        |
+        |
+        |
+        -------
+        """,
+        # 2 incorrect attempts:
+        """
+        -------
+        |   |
+        |   O
+        |  ---
+        |   |
+        |   |
+        |
+        |
+        |
+        |
+        -------
+        """,
+        # 3 incorrect attempts:
+        """
+        -------
+        |   |
+        |   O
+        |  ---
+        | / |
+        |   |
+        |
+        |
+        |
+        |
+        -------
+        """,
+        # 4 incorrect attempts:
+        """
+        -------
+        |   |
+        |   O
+        |  ---
+        | / | \
+        |   |
+        |
+        |
+        |
+        |
+        -------
+        """,
+        # 5 incorrect attempts:
+        """
+        -------
+        |   |
+        |   O
+        |  ---
+        | / | \
+        |   |
+        |  ---
+        | /
+        |
+        |
+        -------
+        """,
+        # 6 incorrect attempts:
+        """
+        -------
+        |   |
+        |   O
+        |  ---
+        | / | \
+        |   |
+        |  ---
+        | /   \
+        |
+        |
+        -------
+        """
+    ]
+
+    print(hangman_graphics[incorrect_attempts])
